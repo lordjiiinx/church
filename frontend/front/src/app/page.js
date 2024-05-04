e1ed5b197a557ee6b3fafdafb61db7673c4306f0
lordjiiinx <lordjiiinx@gmail.com>
2024-05-04 10:58:11 +0300

    ini

--- frontend/front/src/app/page.js
+++ frontend/front/src/app/page.js
@@ -2,7 +2,28 @@
 
 export default function Home() {
   return (
+    <div className="md:container md:mx-auto grid grid-cols-1 md:grid-cols-2">
+
+      <div>
+        hi
+
+      </div>
+
+      <div>
+        wooza
+      </div>
+
+      <div>
+        cazzama
+      </div>
+
+
+
+
+
+
+    </div>
   
-    <p>hello</p>
+    
   );
 }
